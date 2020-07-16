2f264c2740f7859ddbc11163a5b9f56d94e8a7cb
GabrielMVzla <gabriel_montesvzla@hotmail.com>
2020-07-15 18:10:59 -0600

    1ras_4_Letras_Curp
    Ahora con llamar el procedimiento main(). te pedira los datos de tu(s) nombre(s) y apellidos para desplegar las primeras 4 letras de una curp

--- Curp.pl
+++ Curp.pl
@@ -7,16 +7,34 @@ jose(jose).
 maria(maria).
 
 
+main():-
+	primerApellido(PAV, PAC),
+	segundoApellido(SA),
+	nombre(N),
+%Incluir a futuro conversor a letra mayusculas y si ya son mayusculas comprobar
+	write(PAC),
+	write(PAV),
+	write(SA),
+	write(N).
+
 primerVocal([], _).
 primerVocal([C | R], V) :- vocal(C), V = C, !. 
 primerVocal([C | R], V) :- not(vocal(C)), primerVocal(R, V).
 
-prueba(S, L) :-
+primerApellido(S, L) :-
 	write("Ingresa tu primer apellido: "),
-	read(C), 
-	string_chars(C, [L | R]),
+	readln(C),
+	charName(C, L, R),
 	primerVocal(R, S).
-	
+%este charName es solo para primer apellido
+charName([Texto|_], L, R):- string_chars(Texto, [L | R]).
+
+segundoApellido(L):-
+	write("Ingresa tu segundo apellido: "),
+	readln(C),
+	charName2doAp(C, L).
+%este charName es solo para segundo apellido
+charName2doAp([Texto|_], L):- string_chars(Texto, [L|_]).
 
 nombre(Res):-write("Ingresa nombre(s): "),
 	readln(T), enlistar(T, Res).
@@ -26,6 +44,6 @@ enlistar([T|_], Res):- not(jose(T)), not(maria(T)), charName(T, Res).
 
 %por si el primer nombre es josé o maría descarta el proc de arriba, se manda a llamar el este y saca la letra del 2do nombre
 enlistar([_,Y|_], Res):- charName(Y, Res).
-
+%solo será para el nombre este proc de charName
 charName(Texto, Res):- string_chars(Texto, [L | _]), Res = L, !.
 
